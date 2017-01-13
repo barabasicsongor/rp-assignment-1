85200e5cfe9d9c02978598ea81ac5595a395a87d
Csongor Barabasi <barabasi.csongi96@gmail.com>
2017-01-13 21:05:39 +0000

    Initial project commit

---
+++ src/rp/assignments/individual/ex1/PentagonController.java
@@ -0,0 +1,35 @@
+package rp.assignments.individual.ex1;
+
+import rp.robotics.DifferentialDriveRobot;
+import rp.robotics.MobileRobot;
+import rp.systems.StoppableRunnable;
+
+/**
+ * 
+ * A placeholder to show you how you could start writing a controller for the
+ * first part of the first individual assignment (creating a controller which
+ * drives in a pentagon). Note that you don't have to follow this structure for
+ * your controller as there are more elegant and efficient (at least in terms of
+ * numbers of lines of code) in which you can implement the different shape
+ * controllers.
+ * 
+ * @author Nick Hawes
+ *
+ */
+public class PentagonController implements StoppableRunnable {
+
+	public PentagonController(DifferentialDriveRobot robot, float sideLength) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+	}
+
+}
