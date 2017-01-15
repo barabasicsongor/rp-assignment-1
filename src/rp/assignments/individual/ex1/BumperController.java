918ec562f98c3c865070c04a5127b3997d38af66
Csongor Barabasi <barabasi.csongi96@gmail.com>
2017-01-15 01:40:13 +0000

    Bumper Controller

---
+++ src/rp/assignments/individual/ex1/BumperController.java
@@ -0,0 +1,71 @@
+package rp.assignments.individual.ex1;
+
+import lejos.robotics.navigation.Pose;
+import rp.robotics.DifferentialDriveRobot;
+import rp.robotics.TouchSensorEvent;
+import rp.systems.ControllerWithTouchSensor;
+
+public class BumperController implements ControllerWithTouchSensor {
+
+	private final DifferentialDriveRobot robot;
+	private boolean robot_run;
+	private boolean robot_bumped;
+
+	public BumperController(DifferentialDriveRobot robot) {
+		this.robot = robot;
+		this.robot_run = true;
+		this.robot_bumped = false;
+	}
+
+	@Override
+	public void stop() {
+		robot_run = false;
+	}
+
+	@Override
+	public void run() {
+		while (this.robot_run) {
+
+			try {
+
+				if (this.robot_bumped) {
+					this.robot.getDifferentialPilot().stop();
+					
+					if(this.robot.getPose().getHeading() == 180.0) {
+						this.robot.setPose(new Pose(this.robot.getPose().getX(), this.robot.getPose().getY(), 0.0f));
+					} else {
+						this.robot.setPose(new Pose(this.robot.getPose().getX(), this.robot.getPose().getY(), 180.0f));
+					}
+					
+					this.robot_bumped = false;
+				}
+				
+				this.robot.getDifferentialPilot().forward();
+
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		this.robot.getDifferentialPilot().stop();
+	}
+
+	@Override
+	public void sensorPressed(TouchSensorEvent _e) {
+		// TODO Auto-generated method stub
+		this.robot_bumped = true;
+	}
+
+	@Override
+	public void sensorReleased(TouchSensorEvent _e) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void sensorBumped(TouchSensorEvent _e) {
+		// TODO Auto-generated method stub
+	}
+
+}
